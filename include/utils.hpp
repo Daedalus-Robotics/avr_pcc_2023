bd3d2d35da5fd3bbee3490106d9669feff114454
literallyanything <hunterbaker@me.com>
2023-08-24 00:32:03 -0400

    Add documentation to utils

--- include/utils.hpp
+++ include/utils.hpp
@@ -8,6 +8,9 @@
 #define AVR_PCC_2023_UTILS_HPP
 
 
+/**
+ * Reset the microcontroller
+ */
 void reset();
 
 enum LogLevel
@@ -19,10 +22,24 @@ enum LogLevel
     FATAL = 50
 };
 
+/**
+ * Send a log message on a ros topic
+ * @param level The log level
+ * @param msg The log message to be sent
+ */
 void log(LogLevel level, const char msg[]);
 
+/**
+ * Sets up the logger node and its publisher
+ * @param support The support structure that is needed to init a node
+ */
 void initLogger(rclc_support_t* support);
 
+/**
+ * Checks the error code passed in and logs if it is an issue
+ * @param error The error code
+ * @param do_reset Whether to reset the microcontroller if there is an error
+ */
 void handleError(rcl_ret_t error, bool do_reset = false);
 
 
